0f671e4bbfd5d41c185b2bcddee6036861fbb0c6
cmj <cmj@cmj.tw>
2020-03-06 17:37:11 +0800

    [ETF] Support local cache for the query result

--- twetf/twetf.go
+++ twetf/twetf.go
@@ -7,18 +7,21 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/user"
 	"regexp"
 	"strings"
+
+	"gopkg.in/yaml.v2"
 )
 
 type ETF struct {
-	*TWSE
+	*TWSE `yaml:"-"`
 
-	ListingDate string
-	ID          string
-	Name        string
-	Issuer      string
-	Link        string
+	ListingDate string `yaml:"listing_date"`
+	ID          string `yaml:"id"`
+	Name        string `yaml:"name"`
+	Issuer      string `yaml:"issuer"`
+	Link        string `yaml:"link"`
 }
 
 func MustNewETF(id string) (etf *ETF) {
@@ -90,16 +93,62 @@ func (etf *ETF) parseHTML(pattern, html string) (out string) {
 	return
 }
 
+type TWSECache struct {
+	List map[string][]*ETF	`yaml:"etf"`
+}
+
+func LoadCache(file string) (out *TWSECache, err error) {
+	var data []byte
+
+	out = &TWSECache {
+		List: make(map[string][]*ETF, 0),
+	}
+
+	if data, err = ioutil.ReadFile(file); err != nil {
+		/* Cannot read file */
+		return
+	}
+	err = yaml.Unmarshal(data, &out)
+	return
+}
+
+func (cache *TWSECache) StoreCache(file string) (err error) {
+	var data []byte
+
+	if data, err = yaml.Marshal(&cache); err != nil {
+		/* Cannot marshal the structure to yaml */
+		return
+	}
+
+	err = ioutil.WriteFile(file, data, 0600)
+	return
+}
+
 type TWSE struct {
 	*http.Client
 	*log.Logger
+	*TWSECache
+
+	CacheFile string
+	Force bool
 }
 
 func MustNew() (twse *TWSE) {
 	twse = &TWSE{
 		Client: &http.Client{},
 		Logger: log.New(os.Stderr, "", log.Lshortfile|log.LUTC),
+
+		Force: false,
 	}
+
+	usr, err := user.Current()
+	if err != nil {
+		twse.Panicf("Cannot get current user")
+		return
+	}
+	/* always load the configuration */
+	twse.CacheFile = fmt.Sprintf("%s/.twse", usr.HomeDir)
+	twse.TWSECache, _ = LoadCache(twse.CacheFile)
 	return
 }
 
@@ -119,6 +168,11 @@ func (twse *TWSE) Types() (out []string) {
 func (twse *TWSE) List(filter string) (list []*ETF) {
 	URL := ""
 	offset := 0
+
+	if data, ok := twse.TWSECache.List[filter]; ok && ! twse.Force && len(data) != 0 {
+		list = data
+		return
+	}
 
 	switch filter {
 	case "domestic":
@@ -173,46 +227,84 @@ func (twse *TWSE) List(filter string) (list []*ETF) {
 			Name:   string(columns[offset+1][1]),
 		})
 	}
+
+	twse.TWSECache.List[filter] = list
+	twse.TWSECache.StoreCache(twse.CacheFile)
 	return
 }
 
-func usage() {
-	fmt.Printf("Usage: %s ACTION [OPTION]\n", os.Args[0])
-	fmt.Printf("\n")
-	fmt.Printf("    help    Show this message\n")
-	fmt.Printf("    type    List all type of ETF\n")
-	fmt.Printf("    list    Get the ETF ID and Name\n")
+func (twse *TWSE) Parse(name string, args... string) {
+	actions := []string{}
+
+	for idx := 0; idx < len(args); idx ++ {
+		switch args[idx] {
+		case "-f":
+			if idx ++; idx >= len(args) {
+				twse.Usage(name, "-f option need file path")
+			}
+		case "-F", "--force":
+			twse.Force = true
+			break
+		default:
+			actions = append(actions, args[idx])
+		}
+	}
+
+	if len(actions) == 0 {
+		twse.Usage(name)
+		return
+	}
+
+	switch actions[0] {
+		case "type":
+			result := twse.Types()
+			fmt.Printf("%s\n", strings.Join(result, " "))
+			break
+		case "list":
+			var filter string
+
+			if filter = ""; len(actions) > 1 {
+				filter = actions[1]
+			}
+
+			for _, etf := range twse.List(filter) {
+				fmt.Printf("%s\n", etf)
+			}
+			break
+		default:
+			twse.Usage(name)
+			break
+	}
+}
+
+func (twse *TWSE) Usage(name string, err ...interface{}) {
+	/* disable the debug log format */
+	twse.SetFlags(0)
+	if len(err) >0 {
+		for _, e := range err {
+			twse.Printf("error: %v\n", e)
+		}
+	}
+
+	twse.Printf("Usage: %s [OPTION] ACTION\n", name)
+	twse.Printf("\n")
+	twse.Printf("ACTION\n")
+	twse.Printf("    help           Show this message\n")
+	twse.Printf("    type           List all type of ETF\n")
+	twse.Printf("    list           Get the ETF ID and Name\n")
+	twse.Printf("OPTION\n")
+	twse.Printf("    -f FILE        The cache file (default: ~/.twetf)\n")
+	twse.Printf("    -F, --force    Force fetch\n")
 	os.Exit(1)
 }
 
 func main() {
-	var action string
-	var option string
-
-	if action = ""; len(os.Args) > 1 {
-		action = os.Args[1]
-	}
-	if option = ""; len(os.Args) > 2 {
-		option = os.Args[2]
-	}
-
-	switch action {
-	case "", "help":
-		usage()
-	case "type":
-		twse := MustNew()
-		fmt.Printf("%s\n", strings.Join(twse.Types(), ", "))
-		break
-	case "list":
-		twse := MustNew()
-		for _, etf := range twse.List(option) {
-			fmt.Printf("%v\n", etf)
+	twse := MustNew()
+	defer func() {
+		if r := recover(); r != nil {
+			twse.Usage(os.Args[0], r)
 		}
-		break
-	default:
-		fmt.Printf("Unknown action %s\n", action)
-		break
-	}
+	}()
 
-	return
+	twse.Parse(os.Args[0], os.Args[1:]...)
 }
